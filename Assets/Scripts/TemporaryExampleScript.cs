06a4a0b64384310b604d7d06d74466b9f661c34e
aviv1620 <44925669+aviv1620@users.noreply.github.com>
2020-12-30 23:14:45 +0200

    make animation need understand the state machine and delete timer script

---
+++ Assets/Scripts/TemporaryExampleScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animation))]
+public class TemporaryExampleScript : MonoBehaviour
+{
+    private Animation anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("Catch");
+            anim.Play("Catch");
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Debug.Log("Thrown");
+            anim.Play("Thrown");
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Walk");
+            anim.Play("Walk");        
+        }
+
+    }
+}
